b0046659be8edb79d06f1e5f011eae9196fee59a
OnyangoOdipo <shadrackonyango30@gmail.com>
2024-08-11 20:04:44 +0300

    Updated DB

--- includes/nema_portal.sql
+++ includes/nema_portal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 08, 2024 at 09:18 AM
+-- Generation Time: Aug 11, 2024 at 07:04 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -18,8 +18,75 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `task_test`
+-- Database: `nema_portal`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer_queries`
+--
+
+CREATE TABLE `customer_queries` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `issue` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `customer_queries`
+--
+
+INSERT INTO `customer_queries` (`id`, `user_id`, `issue`) VALUES
+(1, 7, '12345');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `documents`
+--
+
+CREATE TABLE `documents` (
+  `id` int(11) NOT NULL,
+  `license_id` int(11) NOT NULL,
+  `file_path` varchar(255) NOT NULL,
+  `uploaded_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `documents`
+--
+
+INSERT INTO `documents` (`id`, `license_id`, `file_path`, `uploaded_at`) VALUES
+(1, 1, 'uploads/._Convolutional Neural Networks in Trading with Python.ipynb', '2024-08-09 16:15:40'),
+(2, 2, 'uploads/._Convolutional Neural Networks in Trading with Python.ipynb', '2024-08-09 16:16:37'),
+(3, 3, 'uploads/._Convolutional Neural Networks in Trading with Python.ipynb', '2024-08-09 16:16:43'),
+(4, 4, 'uploads/._Convolutional Neural Networks in Trading with Python.ipynb', '2024-08-09 16:17:13'),
+(5, 5, '../uploads._Convolutional Neural Networks in Trading with Python.ipynb', '2024-08-09 16:17:42'),
+(6, 6, '../uploads/._Convolutional Neural Networks in Trading with Python.ipynb', '2024-08-11 09:09:47');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `licenses`
+--
+
+CREATE TABLE `licenses` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `license_type` enum('business','environmental') NOT NULL,
+  `application_status` enum('pending','approved','rejected') DEFAULT 'pending',
+  `submitted_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `tracking_code` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `licenses`
+--
+
+INSERT INTO `licenses` (`id`, `user_id`, `license_type`, `application_status`, `submitted_at`, `tracking_code`) VALUES
+(1, 4, 'business', 'pending', '2024-08-09 16:15:40', ''),
+(6, 2, 'environmental', 'pending', '2024-08-11 09:09:47', 'HJcUzSKE');
 
 -- --------------------------------------------------------
 
@@ -91,11 +158,59 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `username`, `password`, `role`) VALUES
 (1, 'rashford09', '$2y$10$2ZavhrBE2f1MjE4C9F/3E.El5SC4lTdq5YwdqZFuuAm12e1p6vREq', 'intern'),
 (2, 'Optimus Prime', '$2y$10$L7PHXYpxoCVBmI..Fxi3UuymV96/YOYw5b4C0Ki6DGnat8Ir0s9CG', 'supervisor'),
-(3, 'sanjay', '$2y$10$ScFPorfC3HaqHrSpiAvFtOjFqakH.gSZM5Iok.f/Jk8b00eqRr.JC', 'intern');
+(3, 'sanjay', '$2y$10$ScFPorfC3HaqHrSpiAvFtOjFqakH.gSZM5Iok.f/Jk8b00eqRr.JC', 'intern'),
+(4, 'intern', '$2y$10$EgiKvKESCBsxRgPyQ1si0u844dAMnrX/GThmv2fYy4uI2ouTEjSCi', 'intern'),
+(5, 'admin@admin.com', '$2y$10$Z6FNgR1uaRRtJ9seeDNx0OZ3bgEymCW5R6BQA4sprxSl3R/.Gsi0a', 'supervisor'),
+(6, 'admin', '$2y$10$.V.PWVP8opAzTvlmzrdZIuJzYXMSHeIkHdhhVqIRO4rcmdzVT/aZG', 'supervisor'),
+(7, 'supervisor', '$2y$10$F/eDIklB/5w93hzVM6/iruMEehmwWePggFoJvctXAGQCKzNqkJPsW', 'supervisor');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_logs`
+--
+
+CREATE TABLE `user_logs` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `login_time` datetime NOT NULL,
+  `logout_time` datetime DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_logs`
+--
+
+INSERT INTO `user_logs` (`id`, `user_id`, `login_time`, `logout_time`, `created_at`) VALUES
+(1, 4, '2024-08-11 18:52:08', '2024-08-11 19:52:19', '2024-08-11 16:52:19'),
+(2, 7, '2024-08-11 18:52:54', NULL, '2024-08-11 16:52:54');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `customer_queries`
+--
+ALTER TABLE `customer_queries`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `documents`
+--
+ALTER TABLE `documents`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `license_id` (`license_id`);
+
+--
+-- Indexes for table `licenses`
+--
+ALTER TABLE `licenses`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `tracking_code` (`tracking_code`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `logs`
@@ -121,8 +236,33 @@ ALTER TABLE `users`
   ADD UNIQUE KEY `username` (`username`);
 
 --
+-- Indexes for table `user_logs`
+--
+ALTER TABLE `user_logs`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `customer_queries`
+--
+ALTER TABLE `customer_queries`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `documents`
+--
+ALTER TABLE `documents`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `licenses`
+--
+ALTER TABLE `licenses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `logs`
@@ -140,11 +280,35 @@ ALTER TABLE `tasks`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `user_logs`
+--
+ALTER TABLE `user_logs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `customer_queries`
+--
+ALTER TABLE `customer_queries`
+  ADD CONSTRAINT `customer_queries_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `documents`
+--
+ALTER TABLE `documents`
+  ADD CONSTRAINT `documents_ibfk_1` FOREIGN KEY (`license_id`) REFERENCES `licenses` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `licenses`
+--
+ALTER TABLE `licenses`
+  ADD CONSTRAINT `licenses_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `logs`
@@ -159,6 +323,12 @@ ALTER TABLE `logs`
 ALTER TABLE `tasks`
   ADD CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`supervisor_id`) REFERENCES `users` (`id`),
   ADD CONSTRAINT `tasks_ibfk_2` FOREIGN KEY (`intern_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `user_logs`
+--
+ALTER TABLE `user_logs`
+  ADD CONSTRAINT `user_logs_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
